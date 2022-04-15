02ff0553a979eebfed8ef1233630c80363b715f9
Sean <craig.sean92@gmail.com>
2022-04-14 23:34:10 -0600

    commit tree 74c94c9797600483578999a49966f5635544029a

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative './user'
+require 'pry'
 
+class Student < User
+    attr_accessor :knowledge
+    attr_writer :learn
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
 end
